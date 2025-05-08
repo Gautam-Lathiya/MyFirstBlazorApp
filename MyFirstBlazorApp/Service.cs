97da5bcbc867a4882abc5e9d8672bb443a1405fd
Gautam-Lathiya <gautamlathiya18@gmail.com>
2025-05-08 19:09:44 +0530

    Add project files.

---
+++ MyFirstBlazorApp/Service.cs
@@ -0,0 +1,12 @@
+﻿namespace MyFirstBlazorApp
+{
+    public class Service
+    {
+        public List<string> list = new List<string>();
+
+        public void Add(string item)
+        {
+            list.Add(item);
+        }
+    }
+}
